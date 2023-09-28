7d2a84437c3a757341e25136a781d0d11df61e5c
Spencer Mattison <spenceramattison@gmail.com>
2023-09-28 17:34:35 +0900

    Change Post routes to api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  # API routes
+  namespace :api do
+    namespace :v1 do
+      resource :posts
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
